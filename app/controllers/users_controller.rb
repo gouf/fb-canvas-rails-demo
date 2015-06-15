824508bf80edff741b78b1d77e13bb5a6c4cc1d5
Gou Furuya <innocent.zero@gmail.com>
2015-06-16 01:11:38 +0900

    Allow display in iframe
    * [responsive design - Cannot display my rails 4 app in iframe even if 'X-Frame-Options' is 'ALLOWALL' - Stack Overflow](http://stackoverflow.com/questions/17542511/cannot-display-my-rails-4-app-in-iframe-even-if-x-frame-options-is-allowall)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
+  before_filter :allow_iframe_requests
   skip_before_filter :verify_authenticity_token, :only => [:index]
+
   def index
+  end
+
+  def allow_iframe_requests
+    response.headers.delete('X-Frame-Options')
   end
 end
